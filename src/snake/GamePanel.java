d7267f0b8c04da29a83471d9ed1b03108c705352
Darius <vasilcandarius50@gmail.com>
2026-05-23 21:13:46 +0300

    feat: implementare game loop, timer si logica de miscare automata a sarpelui

--- src/snake/GamePanel.java
+++ src/snake/GamePanel.java
@@ -6,22 +6,40 @@ import java.awt.Dimension;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GamePanel extends JPanel {
+
+public class GamePanel extends JPanel implements ActionListener {
     static final int Screen_Width = 600;
     static final int Screen_Height = 600;
     static final int Unit_Size = 25;
+    static final int Delay = 150;
 
     List<Point> snake = new ArrayList<>();
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(Screen_Width, Screen_Height));
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
+        startGame();
 
-        for(int i = 0; i < 6;i++) {
+
+    }
+
+    public void startGame() {
+        for (int i = 0; i < 6; i++) {
             snake.add(new Point(150 - (i * Unit_Size), 50));
         }
+
+        running = true;
+
+        timer = new Timer(Delay, this);
+        timer.start();
     }
 
     @Override
@@ -31,21 +49,60 @@ public class GamePanel extends JPanel {
     }
 
     public void deseneaza(Graphics g) {
-        g.setColor(Color.DARK_GRAY);
-        for (int i = 0; i < Screen_Height / Unit_Size;i++) {
-            g.drawLine(i * Unit_Size, 0, i * Unit_Size, Screen_Height);
-            g.drawLine(0, i * Unit_Size, Screen_Width, i * Unit_Size);
-        }
+
+        if (running) {
+            g.setColor(Color.DARK_GRAY);
+            for (int i = 0; i < Screen_Height / Unit_Size; i++) {
+                g.drawLine(i * Unit_Size, 0, i * Unit_Size, Screen_Height);
+                g.drawLine(0, i * Unit_Size, Screen_Width, i * Unit_Size);
+            }
 
 
-        for (int i = 0; i < snake.size(); i++) {
-            if (i == 0) {
-                g.setColor(Color.GREEN);
-                g.fillRect(snake.get(i).x, snake.get(i).y, Unit_Size, Unit_Size);
-            } else {
-                g.setColor(new Color(45, 180, 0));
-                g.fillRect(snake.get(i).x, snake.get(i).y, Unit_Size, Unit_Size);
+            for (int i = 0; i < snake.size(); i++) {
+                if (i == 0) {
+                    g.setColor(Color.GREEN);
+                    g.fillRect(snake.get(i).x, snake.get(i).y, Unit_Size, Unit_Size);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(snake.get(i).x, snake.get(i).y, Unit_Size, Unit_Size);
+                }
             }
         }
     }
+
+    public void move() {
+        Point capCurent = snake.get(0);
+
+        Point capNou = new Point(capCurent.x, capCurent.y);
+
+        switch (direction) {
+            case 'U':
+                capNou.y = capNou.y - Unit_Size;
+                break;
+            case 'D':
+                capNou.y = capNou.y + Unit_Size;
+                break;
+            case 'L':
+                capNou.x = capNou.x - Unit_Size;
+                break;
+            case 'R':
+                capNou.x = capNou.x + Unit_Size;
+                break;
+        }
+
+        snake.add(0, capNou);
+
+        snake.remove(snake.size() - 1);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (running) {
+            move();
+        }
+        repaint();
+    }
 }
+
+
